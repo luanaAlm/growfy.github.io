ecafe3da4a46e67fce18521c20a08ca31f9eefb1
Luana Rodirgues <luanarodrigues3211@gmail.com>
2022-08-30 13:45:42 -0300

    growfy

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    /* ---carousel depoimento--- */
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+})
